2f24b435ce0c583f45e89f30cd3c1c2e7a3e3fbb
Pallavi <pallaviarshanapelli@gmail.com>
2026-05-21 16:00:59 +0530

    Sum Of N Natural Numbers

---
+++ src/JAVAPROBLEMS/SumOfNaturalNumbers.java
@@ -0,0 +1,17 @@
+package JAVAPROBLEMS;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=sc.nextInt();
+        int sum=0;
+        for(int i=0;i<=num;i++){
+            sum=sum+i;
+        }
+        System.out.println("Total Sum of the n natural numbers:" +sum);
+
+    }
+}
