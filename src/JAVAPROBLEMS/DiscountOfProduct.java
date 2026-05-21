422ea371c97fb60d2607e222db1b5f528d99bea1
Pallavi <pallaviarshanapelli@gmail.com>
2026-05-21 15:24:09 +0530

    Discout Of The Product Problem

---
+++ src/JAVAPROBLEMS/DiscountOfProduct.java
@@ -0,0 +1,16 @@
+package JAVAPROBLEMS;
+
+import java.util.Scanner;
+
+public class DiscountOfProduct {
+    public static void main() {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter The Amount:");
+        int Amount=sc.nextInt();
+        System.out.print("Enter The Discount:");
+        int Discount=sc.nextInt();
+        double DiscountAmount=Amount*(Discount/100.0);
+        double bill=Amount-DiscountAmount;
+        System.out.println("Total Amount:" +bill);
+    }
+}
