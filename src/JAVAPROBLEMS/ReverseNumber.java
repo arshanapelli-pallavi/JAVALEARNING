4a14eee593b8a6b54836846cfdaca63056c19ab9
Pallavi <pallaviarshanapelli@gmail.com>
2026-05-21 16:03:45 +0530

    Reversing a Number

---
+++ src/JAVAPROBLEMS/ReverseNumber.java
@@ -0,0 +1,19 @@
+package JAVAPROBLEMS;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n= sc.nextInt();
+        int rev=0;
+        while(n>0){
+            int rem=n%10;
+            n=n/10;
+            rev=rev*10+rem;
+        }
+        System.out.println(rev);
+    }
+}
+
