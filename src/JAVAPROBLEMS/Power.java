48de90e15be954213ae30bb3b3724ce4776d0411
Pallavi <pallaviarshanapelli@gmail.com>
2026-05-21 15:47:13 +0530

    Power Of a Number in Java

---
+++ src/JAVAPROBLEMS/Power.java
@@ -0,0 +1,15 @@
+package JAVAPROBLEMS;
+
+import java.util.Scanner;
+
+public class Power {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int  num=sc.nextInt();
+        System.out.print("Enter The Power:");
+        int power=sc.nextInt();
+        double result=Math.pow(num,power);
+        System.out.println("Power Of Number:"+ result);
+    }
+}
