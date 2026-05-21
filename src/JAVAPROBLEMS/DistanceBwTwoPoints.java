bf14d99e231f7c02fd5190376ec694b0b4b0bec4
Pallavi <pallaviarshanapelli@gmail.com>
2026-05-21 15:39:40 +0530

    Distance Between Two Points Problem

---
+++ src/JAVAPROBLEMS/DistanceBwTwoPoints.java
@@ -0,0 +1,19 @@
+package JAVAPROBLEMS;
+
+import java.util.Scanner;
+
+public class DistanceBwTwoPoints {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter x1:");
+        double x1=sc.nextDouble();
+        System.out.print("enter x2:");
+        double x2=sc.nextDouble();
+        System.out.print("enter y1:");
+        double y1=sc.nextDouble();
+        System.out.print("enter y2:");
+        double y2=sc.nextDouble();
+        double distance=Math.sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)));
+        System.out.println("Total Distance:" +distance);
+    }
+}
